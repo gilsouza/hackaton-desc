7745ebeb77933013ac6fd0f2df43a2e9fef84cf4
Douglas Toledo <douglas22toledo@gmail.com>
2021-08-05 20:34:49 -0300

    duvidas v1

--- front/src/pages/Duvidas/index.jsx
+++ front/src/pages/Duvidas/index.jsx
@@ -1,5 +1,44 @@
-import { PageContainer } from './styles';
+import { useEffect } from 'react';
+import { useCareers } from '../../hooks/Careers';
+import {
+  PageContainer,
+  HorizontalLine,
+  QuestionListItemContainer,
+  QuestionListItemText,
+  QuestionListItemTitle,
+  QuestionListItemUsername,
+} from './styles';
 
-const Duvidas = () => <PageContainer />;
+const Duvidas = () => {
+  const { getQuestions, questions } = useCareers();
+  useEffect(() => {
+    getQuestions();
+  }, []);
+
+  const renderQuestionList = () => {
+    if (!questions.length) { return <div>Essa carreira ainda não tem perguntas!</div>; }
+
+    return questions.map((question, index) => (
+      <div key={question.id} style={{ width: '100%' }}>
+        <QuestionListItemContainer key={question.id}>
+          <QuestionListItemTitle>
+            {question.title}
+          </QuestionListItemTitle>
+          <QuestionListItemText>
+            {question.text}
+          </QuestionListItemText>
+          <QuestionListItemUsername>{question.user.name}</QuestionListItemUsername>
+        </QuestionListItemContainer>
+        {index !== questions.length - 1 && <HorizontalLine />}
+      </div>
+    ));
+  };
+
+  return (
+    <PageContainer>
+      {renderQuestionList()}
+    </PageContainer>
+  );
+};
 
 export { Duvidas };
--- front/src/pages/Duvidas/styles.js
+++ front/src/pages/Duvidas/styles.js
@@ -1,3 +1,38 @@
 import styled from 'styled-components';
 
-export const PageContainer = styled.div``;
+export const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  flex: 1;
+`;
+
+export const HorizontalLine = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: #bbb;
+`;
+
+export const QuestionListItemContainer = styled.div`
+  margin-top: 10px;
+  height: 80px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+export const QuestionListItemTitle = styled.div`
+  font-size: 20px;
+  color: #222;
+`;
+
+export const QuestionListItemText = styled.div`
+  font-size: 14px;
+  color: #555;
+`;
+
+export const QuestionListItemUsername = styled.div`
+  font-size: 10px;
+  color: #777;
+`;
