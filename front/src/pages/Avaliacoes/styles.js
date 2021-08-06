6edec5541a6a7a6abe746c3436592ac433f4007f
Gilmar Souza <gilmar.souza@descomplica.com.br>
2021-08-06 02:12:01 -0300

    feat: page avaliacoes

--- front/src/pages/Avaliacoes/styles.js
+++ front/src/pages/Avaliacoes/styles.js
@@ -1,3 +1,42 @@
 import styled from 'styled-components';
 
 export const PageContainer = styled.div``;
+
+export const TotalResult = styled.div`
+    font-weight: 400;
+    display: flex;
+    align-items: flex-start;
+    height: 20px;
+    width: 100%;
+`;
+
+export const GeneralResults = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 80px;
+    width: 100%;
+    font-size: 20px;
+`;
+
+export const TextResult = styled.span`
+    margin-right: 10px;
+`;
+
+export const LabelResult = styled(TextResult)`
+    margin-right: 10px;
+    font-size: 20px;
+`;
+
+export const BoxRatings = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    flex-direction: column;
+`;
+
+export const Rating = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
