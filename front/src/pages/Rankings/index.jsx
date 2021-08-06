245c0834ffdad01a9e706941853350e5a4bcf0b3
Gustavo <guhcostan@gmail.com>
2021-08-06 16:01:44 -0300

    adding career card

--- front/src/pages/Rankings/index.jsx
+++ front/src/pages/Rankings/index.jsx
@@ -1,8 +1,12 @@
 /* eslint-disable no-console */
 import { useEffect, useMemo } from 'react';
 import { Header } from '../../components/Header';
-import { HeaderText, PageContainer, RakingHeader } from './styles';
+import {
+  ContentContainer, HeaderText, PageContainer, RakingHeader, Row, Title,
+} from './styles';
 import { useCareers } from '../../hooks/Careers';
+import { PageBackground } from '../Profession/styles';
+import { CareerCard } from '../../components/CarrerCard';
 
 const Rankings = () => {
   const { getRatingsWithCareer, ratingsWithCareer } = useCareers();
@@ -17,7 +21,7 @@ const Rankings = () => {
       (rv[x.careerId] = rv[x.careerId] || []).push(x);
       return rv;
     }, {});
-    console.log(grouped);
+    return grouped;
   }, [ratingsWithCareer]);
 
   console.log(groupRatingsByCareer);
@@ -25,11 +29,21 @@ const Rankings = () => {
   return (
     <>
       <Header />
-      <PageContainer>
-        <RakingHeader>
-          <HeaderText>Raking de carreiras</HeaderText>
-        </RakingHeader>
-      </PageContainer>
+      <PageBackground>
+        <PageContainer>
+          <RakingHeader>
+            <HeaderText>Raking de carreiras</HeaderText>
+          </RakingHeader>
+          {groupRatingsByCareer && Object.keys(groupRatingsByCareer).length > 0 && (
+          <ContentContainer>
+            <Title>Top 10 carreiras por salário</Title>
+            <Row>
+              <CareerCard career={groupRatingsByCareer.jornalismo[0].career} property="salary_satisfaction" score={groupRatingsByCareer.jornalismo[0].salary_satisfaction} />
+            </Row>
+          </ContentContainer>
+          )}
+        </PageContainer>
+      </PageBackground>
     </>
   );
 };
