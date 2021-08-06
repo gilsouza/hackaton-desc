20335324be6bd54b5ef788bcd856e16b4543d188
Douglas Toledo <douglas22toledo@gmail.com>
2021-08-06 08:43:59 -0300

    fix average on avalicao

--- front/src/util/math.js
+++ front/src/util/math.js
@@ -1,6 +1,8 @@
 const averagePropInList = (list, property) => {
   if (list.length) {
-    return list.reduce((a, b) => a + b[property], 0) / list.length;
+    const average = list.reduce((a, b) => a + b[property], 0) / list.length;
+
+    return Math.round(average * 10) / 10;
   }
   return 0;
 };
